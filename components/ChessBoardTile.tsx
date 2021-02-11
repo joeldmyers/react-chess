628f60a76552a73efae8e5f987936ac0844e3bdf
Joel <joeldmyers@gmail.com>
2021-02-11 18:12:44 -0500

    wip chess board setup - issue with CSS

---
+++ components/ChessBoardTile.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ChessBoardTile = () => {
+  return <p>TILE</p>;
+};
+
+export default ChessBoardTile;
---
+++ pages/index.js
@@ -0,0 +1,19 @@
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import ChessBoard from "../components/ChessBoard";
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Chess</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>Update</main>
+      <ChessBoard />
+
+      <footer className={styles.footer}></footer>
+    </div>
+  );
+}
